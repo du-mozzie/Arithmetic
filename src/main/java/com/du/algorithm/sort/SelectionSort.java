bf1bd7421eef94d8c8f2f669bd0edd0c2375edcc
Du <2548425238@qq.com>
2022-05-24 23:42:17 +0800

    feat : add SelectionSort

--- src/main/java/com/du/algorithm/sort/SelectionSort.java
+++ src/main/java/com/du/algorithm/sort/SelectionSort.java
@@ -26,13 +26,17 @@ public class SelectionSort {
     }
 
     private static void selectionSort(int[] arr){
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
+            }
+            if (minIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
             }
         }
     }

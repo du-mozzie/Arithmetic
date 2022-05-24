c03233d8c704b1d767db97f91f4c59a385bddcb6
Du <2548425238@qq.com>
2022-05-24 11:19:52 +0800

    feat : add EdgeComparator

--- src/main/java/com/du/dataStructure/graph/Kruskal.java
+++ src/main/java/com/du/dataStructure/graph/Kruskal.java
@@ -69,11 +69,3 @@ class MySet {
         }
     }
 }
-
-// 最小边比较器
-class EdgeComparator implements Comparator<Edge> {
-    @Override
-    public int compare(Edge o1, Edge o2) {
-        return o1.weight - o2.weight;
-    }
-}

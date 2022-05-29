8f3d87eeda6b0acb3a61a6caab4c2dab928802e9
Du <2548425238@qq.com>
2022-05-29 23:22:44 +0800

    KMP

--- src/main/java/com/du/algorithm/string/KMP.java
+++ src/main/java/com/du/algorithm/string/KMP.java
@@ -29,7 +29,7 @@ public class KMP {
         int i2 = 0;
         // 获取str2的next数组  O(M)
         int[] next = getNextArray(str2);
-        // O()
+        // O(N)
         while (i1 < str1.length && i2 < str2.length) {
             if (str1[i1] == str2[i2]) {
                 // 相等时++
